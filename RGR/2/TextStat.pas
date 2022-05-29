fcfc14bd1873e8672e2ae098cdb322eeb9c8ea48
Dmitry Ivanov <daug32@mail.ru>
2022-05-29 14:02:26 +0300

    Cutted off the additional module

--- RGR/2/TextStat.pas
+++ RGR/2/TextStat.pas
@@ -5,14 +5,87 @@ PROCEDURE CollectStat(InpPath: STRING);
 PROCEDURE SaveStat(OutPath: STRING);
 
 IMPLEMENTATION    
-USES 
-  StringHandler, 
-  TreeContainer;
+USES  
+  StringHandler;
+TYPE
+  NodePtr = ^Node; 
+  Node =  RECORD 
+            Word: STRING;
+            Count: INTEGER;
+            LeftTree: NodePtr;
+            RightTree: NodePtr
+          END; 
+VAR 
+  Head: NodePtr;  
+
+PROCEDURE ContructNode(VAR Element: NodePtr; VAR Word: STRING);
+BEGIN
+  NEW(Element); 
+  Element^.Word := Word;
+  Element^.Count := 1;
+  Element^.LeftTree := NIL;
+  Element^.RightTree := NIL
+END;
+
+PROCEDURE InsertWord(Word: STRING);
+VAR   
+  Prev, Curr: NodePtr;               
+  Comparing: INTEGER;  
+BEGIN {SaveElement} 
+  {If the container is empty}
+  IF Head = NIL
+  THEN
+    BEGIN 
+      ContructNode(Head, Word);   
+      EXIT
+    END; 
+                              
+  {Init}
+  Prev := NIL;
+  Curr := Head;  
+  
+  {Insert word with sorting}
+  WHILE Curr <> NIL
+  DO
+    BEGIN
+      Comparing := StringComparer(Word, Curr^.Word);     
+
+      {Given word already exists, so 
+      increase it's count and return}
+      IF Comparing = 0
+      THEN
+        BEGIN    
+          Curr^.Count := Curr^.Count + 1;
+          EXIT
+        END;
+        
+      {Given word is less than the node's word}
+      IF Comparing = -1
+      THEN
+        BEGIN   
+          Prev := Curr;
+          Curr := Curr^.LeftTree;
+          CONTINUE
+        END;  
+                                                               
+      {Given word is greater than the node's word}         
+      Prev := Curr;
+      Curr := Curr^.RightTree
+    END;
+   
+  {Construct new leaf} 
+  ContructNode(Curr, Word); 
+  IF Comparing = -1
+  THEN
+    Prev^.LeftTree := Curr
+  ELSE
+    Prev^.RightTree := Curr
+END; {SaveElement}
     
 PROCEDURE CollectStat(InpPath: STRING);
-VAR {CollectStat}
-  Word: STRING;  
+VAR {CollectStat}  
   Inp: TEXT;
+  Word: STRING; 
 BEGIN    
   ASSIGN(Inp, InpPath);
   RESET(Inp);
@@ -21,8 +94,7 @@ BEGIN
   WHILE NOT EOF(Inp)
   DO 
     BEGIN       
-      Word := ReadWord(Inp);
-  
+      Word := ReadWord(Inp);  
       IF LENGTH(Word) > 0 
       THEN
         InsertWord(ToLower(Word))
@@ -30,11 +102,29 @@ BEGIN
 
   CLOSE(Inp)  
 END; {CollectStat} 
-   
+ 
 PROCEDURE SaveStat(OutPath: STRING);
+VAR 
+  OutFile: TEXT;
+
+PROCEDURE SaveTree(Curr: NodePtr);
+BEGIN                                    
+  IF Curr^.LeftTree <> NIL
+  THEN
+    SaveTree(Curr^.LeftTree);
+  WRITELN(OutFile, Curr^.Word, ' ', Curr^.Count);
+  IF Curr^.RightTree <> NIL
+  THEN
+    SaveTree(Curr^.RightTree)
+END;
+
 BEGIN {SaveStat}
-  SaveConatiner(OutPath);
+  ASSIGN(OutFile, OutPath); 
+  REWRITE(OutFile);
+  SaveTree(Head);
+  CLOSE(OutFile)
 END; {SaveStat}           
  
-BEGIN {TextStat} 
+BEGIN {TextStat}  
+  Head := NIL;
 END. {TextStat}
